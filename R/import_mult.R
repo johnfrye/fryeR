5189d44467570019425ce387f6c9551c9da84642
John Frye <john.frye@gmail.com>
2021-12-14 16:29:48 -0700

    updated import_mult

--- R/import_mult.R
+++ R/import_mult.R
@@ -2,10 +2,10 @@
 #' 
 #' @param ft A fileTable of files to import and stack
 #' @export
-import_mult <- function(ft, ...) {
+import_mult <- function(ft, which_sel = 1) {
   for (i in 1:nrow(ft)){
     ft$file_path[i]
-    df_sel <- import(ft$file_path[i])
+    df_sel <- import(ft$file_path[i], which = which_sel)
     
     if(i == 1){
       df_comb <- df_sel
