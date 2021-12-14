d5b21516228e131621811f296908240da64fff6e
John Frye <john.frye@gmail.com>
2021-12-14 16:05:43 -0700

    added import_mult function

---
+++ R/import_mult.R
@@ -0,0 +1,19 @@
+#' Move a set of columns to the left-hand side of a data frame
+#' 
+#' @param ft A fileTable of files to import and stack
+#' @export
+import_mult <- function(ft) {
+  for (i in 1:nrow(ft)){
+    ft$file_path[i]
+    df_sel <- import(ft$file_path[i])
+    
+    if(i == 1){
+      df_comb <- df_sel
+    } else if (i != 1){
+      df_comb <- df_comb %>% 
+        rbind(df_sel)
+    }
+    print(paste0(i, " of ", nrow(ft)))
+  }
+  df_comb
+}
